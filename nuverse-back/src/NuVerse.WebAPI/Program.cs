9ed589a1103cc997d0da24185a13177bf674342b
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-12-01 17:06:50 +0200

    Adding Contact Controller

--- nuverse-back/src/NuVerse.WebAPI/Program.cs
+++ nuverse-back/src/NuVerse.WebAPI/Program.cs
@@ -2,9 +2,6 @@ using NuVerse.Application;
 using NuVerse.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
