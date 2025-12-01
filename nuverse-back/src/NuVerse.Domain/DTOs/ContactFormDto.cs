46b0868900fcfc8b3ca1329e4e3742b29a1c37f9
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-12-01 17:28:08 +0200

    Adding the form dto

---
+++ nuverse-back/src/NuVerse.Domain/DTOs/ContactFormDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NuVerse.Domain.DTOs
+{
+    public class ContactFormDto
+    {
+        public string? FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address format")]
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Reason is required")]
+        public string? Reason { get; set; }
+    }
+}
